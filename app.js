8d92067bf95e55973f1bfeb971a6d755c39af387
tzhenkai <sglancelot@gmail.com>
2019-09-16 22:27:02 +0800

    Completed part 4a. blog list 1st part

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
