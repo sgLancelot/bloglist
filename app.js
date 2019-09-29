5f4514c6a83e602f29719b375f7a9eb567c6f481
tzhenkai <sglancelot@gmail.com>
2019-09-29 11:01:36 +0800

    Completed part 4d qn 4.18, implemented token-based auth

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 
@@ -23,6 +24,7 @@ app.use(bodyParser.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
